b2a154d1bfc26e2f8946a5257edf806e66485404
Mohaimanul Islam <islam@kodensya.co.jp>
2019-03-08 15:36:24 +0900

    sett up

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,97 @@
 package sample;
 
-public class Controller {
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Controller extends MouseAdapter {
+    int x=0, y=0, width=0, height= 0;
+    Point start = new Point();
+    Point end = new Point();
+
+    Rectangle screen = new Rectangle();
+
+    @FXML
+    private Button sendMsg;
+
+    @FXML
+    private ImageView imgView;
+
+    @FXML
+    public void captureImage(){
+       // Rectangle screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        MouseListener ml;
+        ml = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                start.setLocation(e.getX(),e.getY());
+                System.out.println("pressed at= "+e.getX());
+            }
+        };
+
+        
+
+        Rectangle screen = new Rectangle(start, new Dimension(end.x - start.x, end.y - start.y));
+
+
+
+    }
+
+public void extractImage(){
+    try {
+        BufferedImage screenCapture = new Robot().createScreenCapture(screen);
+        WritableImage myImage = SwingFXUtils.toFXImage(screenCapture, null);
+        imgView.setImage(myImage);
+        ImageIO.write(screenCapture,"jpg", new File("out.jpg"));
+
+
+
+        Image cursor = ImageIO.read(new File("c:/cursor.gif"));
+        int x = MouseInfo.getPointerInfo().getLocation().x;
+        int y = MouseInfo.getPointerInfo().getLocation().y;
+
+        Graphics2D graphics2D = screenCapture.createGraphics();
+        graphics2D.drawImage(cursor, x, y, 16, 16, null); // cursor.gif is 16x16 size.
+        ImageIO.write(screenCapture, "GIF", new File("c:/capture.gif"));
+
+    } catch (AWTException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+}
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        x=e.getX();
+        y=e.getY();
+        start.setLocation(x,y);
+        System.out.println(x);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        Point end = e.getPoint();
+
+        extractImage();
+//         new Rectangle(start, new Dimension(end.x - start.x, end.y
+//                - start.y));
+//        screen.setBounds(start,start, new Dimension(end.x - start.x, end.y
+//                - start.y));
+        //super.mouseReleased(e);
+    }
+
 }
